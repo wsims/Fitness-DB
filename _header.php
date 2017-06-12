89740de66df364fc6daae9a8db14fcec71a05176
wsims <willy.simz@gmail.com>
2017-06-12 16:52:51 -0700

    Adding header

---
+++ _header.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="assets/style.css">
+<html>
+      <header class = "header">
+        <div class="wrapper">
+          <div class = "nav">
+              <a href="login.php">Login | Sign Up</a>
+              <a href="workoutLog.php">Workout Log</a>
+			        <a href="createWorkout.php">Create Workout</a>
+          </div>
+          <div class="logo">
+            <a href="http://web.engr.oregonstate.edu/~simsw/cs340/FitnessDB/">Fitness DB</a>
+          </div>
+        </div>
+      </header>
+</html>
