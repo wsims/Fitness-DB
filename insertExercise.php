fab5431bcb5f7c6578f5440ec109b4bd0feeb077
r3ypoc <pociusr@oregonstate.edu>
2017-06-14 02:15:20 -0700

    fixed wording and page redirects to add more exercises, added done button to finish adding exercises

--- insertExercise.php
+++ insertExercise.php
@@ -4,6 +4,7 @@ $defaultReps=$_POST['defaultReps'];
 $defaultSets=$_POST['defaultSets'];
 $routineID=$_POST['routineID'];
 $exerciseID=$_POST['exerciseID'];
+$name=$_POST['name'];
 
 if(true == true)
 {
@@ -23,8 +24,12 @@ if(true == true)
 	$result = mysqli_query($conn,"INSERT INTO routineExercise (routineExerciseID, defaultReps, defaultSets, routineID, exerciseID) VALUES ('1','".$defaultReps."','".$defaultSets."','".$routineID."','".$exerciseID."')");
 }
 
+// $name1=mysql_query($conn, "SELECT name, routineID FROM Routine WHERE routineID = '$routineID'");
+// $name2=$name1['name'];
 mysqli_close($conn);
+
+//$name = "leg crusher";
 //
-header("Location: http://web.engr.oregonstate.edu/~pociusr/Fitness-DB/createWorkoutStepTwo.php",TRUE,303);
+header("Location: http://web.engr.oregonstate.edu/~pociusr/Fitness-DB/createWorkoutStepTwo.php?name=".$name."",TRUE,303);
 
 ?>
