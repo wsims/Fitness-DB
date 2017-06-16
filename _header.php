c75a69a81276696cbcc38d0385ba1839d5d6c1f6
wsims <willy.simz@gmail.com>
2017-06-16 11:12:54 -0700

    Comments

--- _header.php
+++ _header.php
@@ -4,10 +4,10 @@
       <header class = "header">
         <div class="wrapper">
           <div class = "nav">
-            <?php session_start();
-            if(isset($_SESSION['UserLogined'])): ?>
-              <a href="logout.php">Logout</a>
-              <?php echo "<a>".$_SESSION['UserLogined']."<a>"; ?>
+            <?php session_start(); //Start a session
+            if(isset($_SESSION['UserLogined'])): ?>  <!--Print out the name of the user if they are logged in-->
+              <a href="logout.php">Logout</a>  <!--Display the logout button if a session is active and user is logged in-->
+              <?php echo "<a>".$_SESSION['UserLogined']."<a>"; ?> 
             <?php else: ?>
               <a href="login.php">Log In</a>
               <a href="signup.php">Sign Up</a>
@@ -16,7 +16,7 @@
 			        <a href="createWorkout.php">Create Workout</a>
           </div>
           <div class="logo">
-            <a href="http://web.engr.oregonstate.edu/~simsw/cs340/FitnessDB/home.php">Fitness DB</a>
+            <a href="http://web.engr.oregonstate.edu/~simsw/cs340/FitnessDB/home.php">Fitness DB</a>  <!--Link to the homepage-->
           </div>
         </div>
       </header>
--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
-  session_start();
-  session_unset();
-  session_destroy();
+  session_start(); 
+  session_unset(); //Unset the session
+  session_destroy(); //Destroy the session
   header("Location: http://web.engr.oregonstate.edu/~simsw/cs340/FitnessDB/home.php"); /* Redirect browser */
   exit();
 ?>
