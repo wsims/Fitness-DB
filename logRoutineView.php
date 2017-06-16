7f5b399e68430a8fb9e4c4b6d720b734fabff367
bolanosf <bolanosf@oregonstate.edu>
2017-06-16 01:35:53 -0700

    displays logged excercises

--- logRoutineView.php
+++ logRoutineView.php
@@ -1,5 +1,4 @@
-
-<?php include("_header.php"); 
+<?php include("_header.php");
 	session_start ();
 ?>
 <link rel="stylesheet" href="assets/style.css">
@@ -13,7 +12,7 @@
 	// change the value of $dbuser and $dbpass to your username and password
 		include 'connectvarsEECS.php';
 		$id = $_GET["id"];
-		
+
 		$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 		if (!$conn) {
 			die('Could not connect: ' . mysql_error());
@@ -24,42 +23,42 @@
 		$routineName = htmlspecialchars($obj -> name);
 		echo "<h1>".$routineName."</h1>";
 
-		$query = "SELECT Exercise.name, defaultSets, defaultReps, logRoutineExercise.weight
-FROM Exercise, routineExercise, Routine , logRoutineExercise
-WHERE Exercise.exerciseID = routineExercise.exerciseID 
-AND Routine.routineID = routineExercise.routineID
-AND Routine.routineID = $id AND Routine.routineID = logRoutineExercise.logRoutineID And Exercise.exerciseID = logRoutineExercise.routineExerciseID" ;
+		$query = "SELECT Exercise.name, defaultSets, defaultReps, logRoutineExercise.weight FROM Exercise, routineExercise, Routine , logRoutineExercise, logRoutine
+		WHERE Exercise.exerciseID = routineExercise.exerciseID AND Routine.routineID = routineExercise.routineID
+		AND Routine.routineID = 1 And routineExercise.routineExerciseID = logRoutineExercise.exerciseRoutineID
+		AND logRoutineExercise.logRoutineID = $id AND logRoutine.logRoutineID = logRoutineExercise.logRoutineID" ;
 
 		$result = mysqli_query($conn, $query);
 		if (!$result) {
 			die("Query to show fields from table failed");
 		}
-	
+
 			$count = 0;
-			while($obj = $result -> fetch_object()){ 
+			while($obj = $result -> fetch_object()){
 			$thisExercise = htmlspecialchars($obj -> name);
 			$thisSets = htmlspecialchars($obj -> defaultSets);
 			$thisReps = htmlspecialchars($obj -> defaultReps);
 			$thisWeight = htmlspecialchars($obj -> weight);
-			$count += 1; 
+			$count += 1;
 			echo "<div id='content'>";
 			echo "<h2 id='title'>".$thisExercise."</h2>";
 			if ($thisReps == 1) {
-				echo "<h2 id='length'> x ".$thisReps." Rep</h2>";	
+				echo "<h2 id='length'> x ".$thisReps." Rep</h2>";
 			} else {
 				echo "<h2 id='length'> x ".$thisReps." Reps</h2>";
 			}
 			if ($thisSets == 1) {
-				echo "<h2 id='length'>".$thisSets." Set</h2>";	
+				echo "<h2 id='length'>".$thisSets." Set</h2>";
 			} else {
-				echo "<h2 id='length'>".$thisSets." Sets</h2>";	
+				echo "<h2 id='length'>".$thisSets." Sets</h2>";
 			}
-			echo " Weight: $thisWeight lbs." ; 
+			echo " Weight: $thisWeight lbs." ;
 			echo "</div>";
 			}
-		
+
 		mysqli_free_result($result);
 		mysqli_close($conn);
 	?>
 </div>
 </html>
+
